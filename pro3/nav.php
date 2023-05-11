a5eb895e006dcc5719caa46852f4ae8308382bd2
Bilal200614 <9019098@student.zadkine.nl>
2023-05-11 10:04:05 +0200

    Hoi

--- pro3/nav.php
+++ pro3/nav.php
@@ -5,16 +5,7 @@
     <ul>
       <li><a href="home.php">Home</a></li>
       <li><a href="shop.php">Shop</a></li>
-      <li class="dropdown">
-        	<a href="#" class="dropbtn">Over ons</a>
-        	<div class="dropdown-content">
-          	<a href="filter.php">Abdel</a>
-          	<a href="index.php">Ali</a>
-          	<a href="Azfar.php">Azfar</a>
-		  	<a href="Bilal.php">Bilal</a>
-		  	<a href="Fraishel.php">Fraishel</a>
-        	</div>
-      	</li>
+      <li><a href="Overmij.php">Over mij</a></li>
       <li><a href="contact.php">Contact</a></li>
     </ul>
   </div>
